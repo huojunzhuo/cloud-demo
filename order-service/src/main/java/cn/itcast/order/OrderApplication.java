1e21897b44a5a23305cde7c6bb0a9d27d0059ab9
HJZ <544652836@qq.com>
2023-02-17 09:19:59 +0800

    2.17commit 关于Nanos

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,5 +1,7 @@
 package cn.itcast.order;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -25,5 +27,10 @@ public class OrderApplication {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+//    @Bean
+//    public IRule randomRule(){
+//        return new RandomRule();//修改为随机规则
+//    }
 
 }
